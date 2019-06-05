a88c61987eaa8de6c63c6d004c5fd4f39830f867
beesswag <beesswag@gmail.com>
2019-06-05 11:47:40 +0200

    first branch commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
         
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"> ToDo Dashboard</div>
+                <div class="card-header"> TODO Dashboard Application</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
